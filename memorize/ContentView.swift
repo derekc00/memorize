d9350f7c951d04f79149e2bd1e01f1a11226abb8
Derek Chang <derektc1@uci.edu>
2020-08-14 23:44:17 -0700

    Initial Commit

---
+++ memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  memorize
+//
+//  Created by Derek Chang on 8/14/20.
+//  Copyright © 2020 Derek Chang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
